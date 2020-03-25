5d5975ff747ef0e250fdcb203d31fb55325645a5
Logan Apple <loganapple@hotmail.com>
2020-03-25 11:33:05 -0400

    Added absurd deck.

--- deck_enums.py
+++ deck_enums.py
@@ -3,8 +3,12 @@ from enum import IntEnum
 
 class DeckEnums(IntEnum):
     BASE_DECK = 0
+    CALTECH_DECK = 1
+    ABSURD_DECK = 2
 
 
 DECK_NAMES = {
-    DeckEnums.BASE_DECK : "base_deck.txt"
+    DeckEnums.BASE_DECK : "base_deck.txt",
+    DeckEnums.CALTECH_DECK : "caltech_deck.txt",
+    DeckEnums.ABSURD_DECK : "absurd_deck.txt"
 }
