f4b22b940aab8684a3fb0ea62a15f6da453bd65c
Logan Apple <loganapple@hotmail.com>
2020-04-24 20:44:21 -0400

    Added 'people in chat' deck.

--- deck_enums.py
+++ deck_enums.py
@@ -17,6 +17,7 @@ class DeckEnums(IntEnum):
     FOOD_PACK = 12
     INTERNET_PACK = 13
     NINETIES_PACK = 14
+    PEOPLE_IN_CHAT_DECK = 15
 
 
 DECK_FILENAMES = {
@@ -34,7 +35,8 @@ DECK_FILENAMES = {
     DeckEnums.SCIFI_PACK : "scifi_pack.txt",
     DeckEnums.FOOD_PACK : "food_pack.txt",
     DeckEnums.INTERNET_PACK : "internet_pack.txt",
-    DeckEnums.NINETIES_PACK : "90s_pack.txt"
+    DeckEnums.NINETIES_PACK : "90s_pack.txt",
+    DeckEnums.PEOPLE_IN_CHAT_DECK : "people_in_chat_deck.txt"
 }
 
 DECK_NAMES = {
@@ -52,5 +54,6 @@ DECK_NAMES = {
     DeckEnums.SCIFI_PACK : "Sci-fi Pack",
     DeckEnums.FOOD_PACK : "Food Pack",
     DeckEnums.INTERNET_PACK : "Internet Pack",
-    DeckEnums.NINETIES_PACK : "90s Pack"
+    DeckEnums.NINETIES_PACK : "90s Pack",
+    DeckEnums.PEOPLE_IN_CHAT_DECK : "People in Chat Deck"
 }
